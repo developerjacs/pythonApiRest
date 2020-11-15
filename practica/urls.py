ffcd6739c1a20065e01d4a62aa68e01abf718bad
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2020-11-15 22:30:51 +0100

    Update urls.py
    Lo mio

--- practica/urls.py
+++ practica/urls.py
@@ -4,6 +4,9 @@ from practica import views
 
 urlpatterns = [
     path('api/usuarios', views.getUsuarios),
+    path('api/usuarios/<pk>', views.getUsuarioId),
+    path('api/usuarios/<pk>/publicaciones', views.publicaciones_list),
+    path('api/usuarios/<pk1>/publicaciones/<pk2>', views.getPublicacionId),
     path('api/publicaciones/<pk>/comentarios', views.comentarios_publicacion),
     path('api/publicaciones', views.publicaciones),
 ]
