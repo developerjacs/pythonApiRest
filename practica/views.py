43a7f3685050beefb44eeb49e259f4a27e354477
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2020-11-15 22:31:32 +0100

    Update views.py
    Lo mio

--- practica/views.py
+++ practica/views.py
@@ -43,6 +43,70 @@ def getUsuarios(request):
             return JsonResponse(usuarios_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(usuarios_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#Funciones de Fran
+#api/usuarios/{id}   -> get, update, delete
+#api/usuarios/{id}/publicaciones   -> get, post    FRAN
+#api/usuarios/{id}/publicaciones/{id}   -> get, delete, update
+@api_view(['GET', 'PUT', 'DELETE'])
+def getUsuarioId(request, pk):
+    try:
+        usuario = Usuario.objects.get(pk=pk)
+    except Usuario.DoesNotExist:
+        return JsonResponse({'message': 'The usuario does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        usuario_serializer = UsuarioSerializer(usuario)
+        return JsonResponse(usuario_serializer.data)
+    elif request.method == 'PUT':
+        usuario_data = JSONParser().parse(request)
+        usuario_serializer = UsuarioSerializer(usuario, data=usuario_data)
+        if usuario_serializer.is_valid():
+            usuario_serializer.save()
+            return JsonResponse(usuario_serializer.data)
+        return JsonResponse(usuario_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        usuario.delete()
+        return JsonResponse({'message': 'Usuario was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def publicaciones_list(request, pk):
+    if request.method == 'GET':
+        try:
+            publicaciones = Publicacion.objects.filter(publicador=pk)
+        except Publicacion.DoesNotExist:
+            return JsonResponse({'message': 'Usuario with specified id does not exist'}, status=status.HTTP_204_NO_CONTENT)
+        serializados = PublicacionSerializer(publicaciones, many=True)
+        return JsonResponse(serializados.data, safe=False, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        publicacion = JSONParser().parse(request)
+        publicacion['fecha'] = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+        publicacion['publicador'] = pk
+        publicacion_serializada = PublicacionSerializer(data=publicacion)
+        if publicacion_serializada.is_valid():
+            publicacion_serializada.save()
+            return JsonResponse(publicacion_serializada.data, safe=False, status=status.HTTP_201_CREATED)
+        return JsonResponse(publicacion_serializada.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getPublicacionId(request, pk1, pk2):
+    try:
+        publicaciones_del_usuario = Publicacion.objects.filter(publicador=pk1)
+        publicacion = publicaciones_del_usuario.get(pk=pk2)
+    except Publicacion.DoesNotExist:
+        return JsonResponse({'message': 'The publicacion does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        publicacion_serializer = PublicacionSerializer(publicacion)
+        return JsonResponse(publicacion_serializer.data)
+    elif request.method == 'PUT':
+        publicacion_data = JSONParser().parse(request)
+        publicacion_data['fecha'] = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+        publicacion_serializer = PublicacionSerializer(publicacion, data=publicacion_data)
+        if publicacion_serializer.is_valid():
+            publicacion_serializer.save()
+            return JsonResponse(publicacion_serializer.data)
+        return JsonResponse(publicacion_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        publicacion.delete()
+        return JsonResponse({'message': 'Publicacion was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 # Funciones de Romolo
 @api_view(['GET', 'POST', 'DELETE'])
