60f475e057d2db15002d440d72c32cf2534acdff
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2020-11-20 19:26:16 +0100

    Ahora con lo de german

--- practica/urls.py
+++ practica/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('api/usuarios/<pk1>/publicaciones/<pk2>', views.getPublicacionId),
     path('api/publicaciones/<pk>/comentarios', views.comentarios_publicacion),
     path('api/publicaciones', views.publicaciones),
+    path('api/poiEAlt', views.getPOIEspaciosAlt),
+    path('api/poiEAlt/id/<id>', views.getPOIbyIdEspaciosAlt),
+    path('api/poiEAlt/nombre/<title>', views.getPOIbyTitleEspaciosAlt),
+    path('api/poiCCul', views.getPOICentrosCulturales),
+    path('api/poiCCul/id/<id>', views.getPOIbyIdCentrosCulturales),
+    path('api/poiCCul/nombre/<title>', views.getPOIbyTitleCentrosCulturales)
 ]
