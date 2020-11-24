31debaad271c51a4455aa90ea99061e150057042
elKuston <romolocaponera@uma.es>
2020-11-24 20:51:24 +0100

    jquery y ajax

--- Practica1/urls.py
+++ Practica1/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from practica import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('practica.urls')),
     path('', include('practica2.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
